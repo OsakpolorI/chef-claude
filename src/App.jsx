65143c8abb1497e37657a4e5a8a41308f19d6acd
Osakpolor Idusuyi <osak.idusuyi@gmail.com>
2026-02-16 23:39:27 -0500

    feat: add main/form component

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@
  * and render it here in the App component
  */
 import Header from "./components/Header.jsx"
+import Main from "./components/Main.jsx"
+
 
 export default function App() {
   return (
+    <>
       <Header />
+      <Main />
+    </>
   )
 }
