839ccce4f2cc5b46320283cf8f110ad12180f0ec
Osakpolor Idusuyi <osak.idusuyi@gmail.com>
2026-02-17 18:39:09 -0500

    feat: add dynamic ingredient list items

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,35 @@
+import { useState } from "react"
+
 export default function Main() {
+    const [ingredients, setIngredients] = useState([])
+
+    const ingredientListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
+
+
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form className="add-ingredient-form" onSubmit={handleSubmit}>
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
                 <button>Add ingredient</button>
             </form>
+            <ul>
+                {ingredientListItems}
+            </ul>
         </main>
     )
 }
