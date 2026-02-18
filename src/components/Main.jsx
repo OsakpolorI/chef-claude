261bae1ebf4b960328dc0a700c6bd22868217696
Osakpolor Idusuyi <osak.idusuyi@gmail.com>
2026-02-17 22:57:30 -0500

    add ingredients header and recipe generation button

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 export default function Main() {
     const [ingredients, setIngredients] = useState([])
 
-    const ingredientListItems = ingredients.map(ingredient => (
+    const ingredientsListItems = ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient}</li>
     ))
 
@@ -24,9 +24,17 @@ export default function Main() {
                 />
                 <button>Add ingredient</button>
             </form>
-            <ul>
-                {ingredientListItems}
-            </ul>
+            {ingredients.length > 0 && <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                {ingredients.length > 3 && <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>}
+            </section>}
         </main>
     )
 }
