c831f1e8a62c97b849bf16f0a5ad4b0a69981ee3
Osakpolor Idusuyi <osak.idusuyi@gmail.com>
2026-02-17 22:37:04 -0500

    refactor: change to react standard form handling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,18 +7,15 @@ export default function Main() {
         <li key={ingredient}>{ingredient}</li>
     ))
 
-    function handleSubmit(event) {
-        event.preventDefault()
-        const formData = new FormData(event.currentTarget)
+    function addIngredient(formData) {
         const newIngredient = formData.get("ingredient")
-
         setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
 
 
     return (
         <main>
-            <form className="add-ingredient-form" onSubmit={handleSubmit}>
+            <form className="add-ingredient-form" action={addIngredient}>
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
