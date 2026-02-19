7f4cce86599d5bca9ef1416ad22b70fbcb15b3b5
Osakpolor Idusuyi <osak.idusuyi@gmail.com>
2026-02-18 21:40:02 -0500

    feat: add component for future recipe instructions

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,36 @@
 import { useState } from "react"
 
+import ClaudeRecipe from "./ClaudeRecipe.jsx"
+
 export default function Main() {
+     /**
+     * Challenge: clean up our code!
+     * Let's make a couple new components to make things a
+     * little cleaner. (Notice: I'm not suggesting what we
+     * have now is bad or wrong. I'm mostly finding an excuse
+     * to get in some hands-on practice 🙂)
+     * 
+     * 1. Move the entire recipe <section> into its own
+     *    ClaudeRecipe component
+     * 2. Move the list of ingredients <section> into its
+     *    own IngredientsList component.
+     * 
+     * While you're considering how to structure things, consider
+     * where state is, think about if it makes sense or not to
+     * move it somewhere else, how you'll communicate between
+     * the parent/child components, etc.
+     * 
+     * The app should function as it currently does when you're
+     * done, so there will likely be some extra work to be done
+     * beyond what I've listed above.
+     */
     const [ingredients, setIngredients] = useState([])
+
+    const [recipeShown, setRecipeShown] = React.useState(false)
+    
+    function toggleRecipeShown() {
+        setRecipeShown(prevShown => !prevShown)
+    }
 
     const ingredientsListItems = ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient}</li>
@@ -35,6 +64,7 @@ export default function Main() {
                     <button>Get a recipe</button>
                 </div>}
             </section>}
+            {recipeShown && <ClaudeRecipe />}
         </main>
     )
 }
